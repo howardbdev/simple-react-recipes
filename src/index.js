dc0e3c6c50d12d7f5be6de8ed85138228ec1ec09
howardbdev <howarddevennish@gmail.com>
2019-04-09 13:23:40 -0400

    start to add redux

--- src/index.js
+++ src/index.js
@@ -3,8 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+// import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ******** probably move this to another file **************
+const initialState = []
+
+const reducer = (initialState, action) => {
+  switch (action.type) {
+    case "GET_RECIPES_SUCCESS":
+      return action.recipes
+    default:
+      return initialState;
+  }
+}
+// ************ and export it from there, import it here *****
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // applyMiddleware(thunk)
+)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
