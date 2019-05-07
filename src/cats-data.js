516a167167b3215431342cd7b34be04a1a267792
howardbdev <howarddevennish@gmail.com>
2019-05-07 07:06:59 -0400

    add dummy data

---
+++ src/cats-data.js
@@ -0,0 +1,9 @@
+const cats = [
+  {url: "https://www.pexels.com/photo/grey-and-white-short-fur-cat-104827/", name: "Fluffy"},
+  {url: "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", name: "Peaches"},
+  {url: "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", name: "Spike"},
+  {url: "https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", name: "Herman"},
+  {url: "https://images.pexels.com/photos/38867/pexels-photo-38867.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", name: "Moose"}
+]
+
+export default cats
