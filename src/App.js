1613e061fe059c436554f70886419f4cd6cb8b8d
howardbdev <howarddevennish@gmail.com>
2019-05-07 07:06:42 -0400

    Build out basic App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import RecipeContainerComponent from './RecipeContainerComponent'
-import logo from './logo.svg';
 import './App.css';
+import Cats from './components/Cats'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <RecipeContainerComponent/>
+        <Cats/>
       </div>
     );
   }
